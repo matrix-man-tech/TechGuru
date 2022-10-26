c608144c9098a3ac9e0e5f2f00e9680f0e33d0a9
Rahul P <rahulparakkal98@gmail.com>
2022-10-26 21:59:58 +0530

    post routes updated till delete post

--- backend/routes/posts/postRoutes.js
+++ backend/routes/posts/postRoutes.js
@@ -1,12 +1,28 @@
 const express = require("express")
-const createPostCtrl = require("../../controllers/post/postController")
+const {
+    createPostCtrl, 
+    fetchPostsCtrl, 
+    fetchPostCtrl,
+    updatePostCtrl,
+    deletePostCtrl} = require("../../controllers/post/postController")
 const authMiddleware = require("../../middleware/auth/authMiddleware")
+const { photoUpload,postImgResize } = require("../../middleware/upload/photoUpload")
+
 
 postRoutes = express.Router()
 
-postRoutes.post('/',authMiddleware, createPostCtrl)
+postRoutes.post(
+    '/',
+    authMiddleware,
+    photoUpload.single('image') ,
+    postImgResize,
+    createPostCtrl
+)
 
-
+postRoutes.get('/',fetchPostsCtrl)
+postRoutes.get('/:id',fetchPostCtrl)
+postRoutes.put('/:id',authMiddleware, updatePostCtrl)
+postRoutes.delete('/:id',authMiddleware, deletePostCtrl)
 
 
 module.exports = postRoutes
